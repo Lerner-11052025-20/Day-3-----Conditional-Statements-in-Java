c9cbcdc1e1478d88d7a78b17fa4c3aff799224ac
Deep Sorathiya <deep.professional803@gmail.com>
2025-05-20 08:58:34 +0530

    Create IFstatement.java
    Here i learned the basic of Conditional Statements in java, like 
    if statement & if...else statement

---
+++ IFstatement.java
@@ -0,0 +1,22 @@
+public class IFstatement
+{
+    public static void main(String args[])
+    {
+        int a = 20;
+        
+        if(a >= 18)
+        {
+            System.out.println("Adult");
+        }
+        else
+        {
+            System.out.println("Not an Adult");
+        }
+
+        if(a < 18)
+        {
+            System.out.println("Not an Adult");
+        }
+
+    }
+}
