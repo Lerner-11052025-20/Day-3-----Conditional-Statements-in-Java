b001b0fbf36cca5c1f66e4c460d006081c1e8088
Deep Sorathiya <deep.professional803@gmail.com>
2025-05-26 21:06:09 +0530

    Add files via upload

---
+++ given_ch.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class given_ch 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        if((ch>=65 && ch<=90) || (ch>=97 && ch<=122))    System.out.println("alphabet");
+        else if(ch>=48 && ch<=57)       System.out.println("number");
+        else    System.out.println("special character");
+    }    
+}
